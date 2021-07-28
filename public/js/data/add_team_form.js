b82741d8a1c1d433a66fbf70037fdd6ceff2ce1a
ChandanNaik999 <chandannaik999@gmail.com>
2021-07-28 22:42:04 +0530

    Feature Add: Add a new team

---
+++ public/js/data/add_team_form.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+const ADD_TEAM_FORM = {
+    "name": "Agile team 57",
+    "shortName": "agilenew",
+    "description": "Team spreading awareness about Agile practices at Zwiggy",
+    "members": [
+        {
+            "userId": 123,
+            "email": "john.doe@example.com"
+        }
+    ]
+};
+
+
+export default ADD_TEAM_FORM;
--- public/js/teams_list.js
+++ public/js/teams_list.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app';
-import { addMemberToTeam, fetchTeams, excuseFromTeam } from './services/teams';
+import { addMemberToTeam, fetchTeams, excuseFromTeam, addTeam } from './services/teams';
+import ADD_TEAM_FORM from './data/add_team_form';
 import getAllUsers from './services/user_management';
 import { TOKEN } from './constants';
 
-function populateTeams( teams, users ) {
+let users = [];
+
+function populateTeams( teams ) {
     const meetingsListDiv = document.getElementById( 'teamsList' );
     meetingsListDiv.innerHTML = '';
 
@@ -127,8 +130,9 @@ function init() {
     fetchTeams()
         .then( ( teams ) => {
             getAllUsers()
-                .then( ( users ) => {
-                    populateTeams( teams, users );
+                .then( ( _users ) => {
+                    users = _users;
+                    populateTeams( teams );
                 } )
                 .catch( ( error ) => {
                     alert( error.message );
@@ -138,5 +142,83 @@ function init() {
             alert( error.message );
         } );
 }
+
+function validateEmail( email ) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test( String( email ).toLowerCase() );
+}
+
+function validateTeam( team ) {
+    const re = /@[a-zA-Z\-0-9]+$/;
+    return re.test( String( team ).toLowerCase() );
+}
+
+function attendeeType( attendee ) {
+    let type = '';
+    if ( validateEmail( attendee ) ) {
+        type = 'email';
+    } else if ( validateTeam( attendee ) ) {
+        type = 'team';
+    } else {
+        type = 'none';
+    }
+    return type;
+}
+let myModal = null;
+
+document.getElementById( 'addNewTeamButton' ).addEventListener( 'click', () => {
+    myModal = new bootstrap.Modal( document.getElementById( 'myModal' ) );
+    myModal.show();
+} );
+
+document.getElementById( 'modalDismiss' ).addEventListener( 'click', () => {
+    // const myModal = new bootstrap.Modal( document.getElementById( 'myModal' ) );
+    myModal.hide();
+} );
+
+document.getElementById( 'submitAddTeam' ).addEventListener( 'click', () => {
+    // const myModal = new bootstrap.Modal( document.getElementById( 'myModal' ) );
+    myModal.hide();
+
+    const teamName = document.getElementById( 'inputTeamName' ).value;
+    const teamShortName = document.getElementById( 'inputShortName' ).value;
+    const teamDescription = document.getElementById( 'teamDescription' ).value;
+    const teamMembers = ( ( document.getElementById( 'inputMembers' ).value ).replace( /\s+/g, '' ) ).split( ',' );
+
+    // TODO: Validation
+    const submitJSON = ADD_TEAM_FORM;
+    submitJSON['name'] = teamName;
+    submitJSON['shortName'] = teamShortName;
+    submitJSON['description'] = teamDescription;
+    const attendeesJSON = [];
+
+    for ( let idxAtt = 0; idxAtt < teamMembers.length; idxAtt += 1 ) {
+        const member = teamMembers[idxAtt];
+        switch ( attendeeType( member ) ) {
+        case 'email':
+            for ( let idx = 0; idx < users.length; idx += 1 ) {
+                if ( member.toLowerCase() === users[idx]['email'].toLowerCase() ) {
+                    attendeesJSON.push( {
+                        userId: users[idx]['_id'],
+                        email: users[idx]['email'],
+                    } );
+                    break;
+                }
+            }
+            break;
+        default: break;
+        }
+    }
+    submitJSON['members'] = attendeesJSON;
+
+    addTeam( submitJSON )
+        .then( () => {
+            // TODO: Confirmation message
+            myModal.hide();
+        } )
+        .catch( ( error ) => {
+            alert( error.message );
+        } );
+} );
 
 init();
