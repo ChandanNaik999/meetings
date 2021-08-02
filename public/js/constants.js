45460861febcb97a86b1a4700bc733ef8e9169f0
ChandanNaik999 <chandannaik999@gmail.com>
2021-08-02 13:06:56 +0530

    Minor modification

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = 'http://mymeetingsapp.herokuapp.com/api';
-const API_BASE_URL = 'http://localhost:3000/api';
+const { API_BASE_URL } = process.env;
 
 const TOKEN = 'token';
 const EMAIL = 'email';
